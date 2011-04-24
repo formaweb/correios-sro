f20c926bc6fc1df961dd8ca6cf92027950e61d62
Caio Tarifa <caiotarifa@MacBook-de-Caio-Tarifa.local>
2011-04-24 02:51:19 -0300

    bug fixes for inexistent sro :P

--- lib/correios-sro.rb
+++ lib/correios-sro.rb
@@ -9,22 +9,25 @@ class CorreiosSRO
 
   def self.track(number)
     @xml = open("http://correios-api.appspot.com/yql?numero=#{number}")
-    track = Track.new(number)
+    @xml = Nokogiri::XML(@xml)
 
-    self.parse(track)
+    if @xml.xpath('/results/status').count > 0
+      track = Track.new(number)
+      self.parse(track)
 
-    return track
+      return track
+    else
+      return false
+    end
   end
 
   private
   def self.parse(track)
-    xml = Nokogiri::XML(@xml)
-
-    xml.xpath('/results/status').each do |value|
+    @xml.xpath('/results/status').each do |value|
       track << Status.new.tap { |s|
         s.date = DateTime.strptime(value.search('data').inner_text, '%d/%m/%Y %H:%M')
         s.place = value.search('local').inner_text
-        s.track = value.search('situacao').inner_text
+        s.status = value.search('situacao').inner_text
         s.details = value.search('detalhes').inner_text
       }
     end
